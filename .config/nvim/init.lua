9768c79e55eab5b24d61b4a9a66af30ff5de8245
Ashot Muradian <ashotmuradian@gmail.com>
2025-11-22 15:46:11 +0500

    Update lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.lsp.enable("roslyn_ls")
+vim.lsp.enable("roslyn_ls_lspconfig_based")
 -- vim.lsp.enable("roslyn_ls_source_based")
 -- vim.lsp.enable("roslyn_ls_vscode_based")
