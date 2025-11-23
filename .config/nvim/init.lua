65e2c8e595949d07eb59d8590a9a38452e205cb8
Ashot Muradian <ashotmuradian@gmail.com>
2025-11-23 21:19:15 +0500

    Disable nvim diagnostics by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.diagnostic.enable(false)
+
 vim.lsp.enable("roslyn_ls_lspconfig_based")
 -- vim.lsp.enable("roslyn_ls_source_based")
 -- vim.lsp.enable("roslyn_ls_vscode_based")
