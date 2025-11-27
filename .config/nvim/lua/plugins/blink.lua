d8ebe52b119eea5d503f51f6c18cec6db0e25c6c
Ashot Muradian <ashotmuradian@gmail.com>
2025-11-27 19:03:28 +0500

    Disable all completion snippets

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,43 +1,51 @@
 return {
-	"saghen/blink.cmp",
-	opts = {
-		completion = {
-			accept = {
-				auto_brackets = {
-					enabled = false,
-					kind_resolution = {
-						enabled = false,
-					},
-					semantic_token_resolution = {
-						enabled = false,
-					},
-				},
-			},
-			documentation = {
-				auto_show = false,
-				window = {
-					border = "rounded",
-				},
-			},
-			ghost_text = {
-				show_without_menu = false,
-			},
-			list = {
-				selection = {
-					auto_insert = false,
-					preselect = false,
-				},
-			},
-			menu = {
-				auto_show = false,
-				border = "rounded",
-			},
-		},
-		signature = {
-			enabled = false,
-			window = {
-				border = "rounded",
-			},
-		},
-	},
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = false,
+          kind_resolution = {
+            enabled = false,
+          },
+          semantic_token_resolution = {
+            enabled = false,
+          },
+        },
+      },
+      documentation = {
+        auto_show = false,
+        window = {
+          border = "rounded",
+        },
+      },
+      ghost_text = {
+        show_without_menu = false,
+      },
+      list = {
+        selection = {
+          auto_insert = false,
+          preselect = false,
+        },
+      },
+      menu = {
+        auto_show = false,
+        border = "rounded",
+      },
+    },
+    signature = {
+      enabled = false,
+      window = {
+        border = "rounded",
+      },
+    },
+    sources = {
+      transform_items = function(_, items)
+        -- https://cmp.saghen.dev/configuration/snippets.html
+        return vim.tbl_filter(function(item)
+          return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
+        end, items)
+      end,
+    },
+  },
 }
