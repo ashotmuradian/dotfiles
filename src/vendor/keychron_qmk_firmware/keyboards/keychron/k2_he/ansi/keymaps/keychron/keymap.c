ad961e1fd2c5c5c263b5853c93157a44fbb921a5
Ashot Muradian <ashotmuradian@gmail.com>
2025-12-28 22:31:55 +0500

    Cleanup

--- src/vendor/keychron_qmk_firmware/keyboards/keychron/k2_he/ansi/keymaps/keychron/keymap.c
+++ src/vendor/keychron_qmk_firmware/keyboards/keychron/k2_he/ansi/keymaps/keychron/keymap.c
@@ -18,10 +18,10 @@
 #include "keychron_common.h"
 
 enum layers {
-    MAC_BASE,
-    MAC_FN,
-    WIN_BASE,
-    WIN_FN,
+    FN0, // fallback layers
+    FN1,
+    FN2, // home row mod layers
+    FN3,
     FN4,
     FN5,
     FN6,
@@ -30,12 +30,9 @@ enum layers {
     FN9,
 };
 
-#define FN_MAC MO(MAC_FN)
-#define FN_WIN MO(WIN_FN)
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC_BASE] = LAYOUT_ansi_84(
+    [FN0] = LAYOUT_ansi_84(
         KC_ESC,   KC_F1,               KC_F2,               KC_F3,               KC_F4,               KC_F5,        KC_F6,        KC_F7,               KC_F8,               KC_F9,               KC_F10,              KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,  UG_TOGG,
         KC_GRV,   MT(MOD_LCTL, KC_1),  MT(MOD_LSFT, KC_2),  MT(MOD_LALT, KC_3),  MT(MOD_LGUI, KC_4),  LT(1, KC_5),  LT(6, KC_6),  MT(MOD_RGUI, KC_7),  MT(MOD_RALT, KC_8),  MT(MOD_RSFT, KC_9),  MT(MOD_RCTL, KC_0),  KC_MINS,  KC_EQL,   KC_BSPC,           KC_PGUP,
         KC_TAB,   KC_Q,                KC_W,                KC_E,                KC_R,                KC_T,         KC_Y,         KC_U,                KC_I,                KC_O,                KC_P,                KC_LBRC,  KC_RBRC,  KC_BSLS,           KC_PGDN,
@@ -43,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,                       KC_Z,                KC_X,                KC_C,                KC_V,         KC_B,         KC_N,                KC_M,                KC_COMM,             KC_DOT,              KC_SLSH,            KC_RSFT,  KC_UP,   KC_END,
         KC_LCTL,  KC_LGUI,             KC_LALT,                                                                     KC_SPC,                                                                      KC_RALT,             KC_RGUI,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT),
 
-    [MAC_FN] = LAYOUT_ansi_84(
+    [FN1] = LAYOUT_ansi_84(
         KC_CAPS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  UG_VALD,  UG_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_PAUS,  NK_TOGG,  UG_NEXT,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            BT_HST1,
         _______,  _______,  UG_VALU,  UG_HUEU,  UG_SATU,  UG_SPDU,  KC_INS,   KC_HOME,  KC_END,   KC_PGDN,  KC_PGUP,  _______,  _______,  _______,            BT_HST2,
@@ -51,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  KC_BSPC,  KC_DEL,   _______,  _______,            _______,  _______,  P2P4G,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  BAT_LVL),
 
-    [WIN_BASE] = LAYOUT_ansi_84(
+    [FN2] = LAYOUT_ansi_84(
         KC_ESC,         KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,        KC_F6,        KC_F7,        KC_F8,        KC_F9,        KC_F10,          KC_F11,   KC_F12,   KC_PSCR,        KC_DEL,   UG_TOGG,
         KC_GRV,         KC_1,         KC_2,         KC_3,         KC_4,         KC_5,         KC_6,         KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS,  KC_EQL,   KC_BSPC,                  KC_PGUP,
         KC_TAB,         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,  KC_RBRC,  KC_BSLS,                  KC_PGDN,
@@ -59,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         OSM(MOD_LSFT),                KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,          KC_SLSH,            OSM(MOD_RSFT),  KC_UP,    KC_END,
         KC_LCTL,        KC_LGUI,      KC_LALT,                                                KC_SPC,                                                 KC_RALT,         KC_RGUI,  KC_RCTL,  KC_LEFT,        KC_DOWN,  KC_RGHT),
 
-    [WIN_FN] = LAYOUT_ansi_84(
+    [FN3] = LAYOUT_ansi_84(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
